488f6a6bed3b0be4d66bd74893d0ce85e1207e71
ETK486 <keerthivaasankarthi@gmail.com>
2023-11-05 23:57:45 +0530

    Many Changes in the Program

--- Expression tree/Expression.cpp
+++ Expression tree/Expression.cpp
@@ -1,61 +1,80 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-class exptree{
-	public:
-		char array[100];
-		char stack[10];
-		int top=-1;
-		int k=0;
-		void root(char val){
-			array[0]=val;
-		}
-		 void exp(char data){
-			if (data=='+'||data=='-'||data=='*'||data=='/'){
-				if (top==-1){
-					cout<<"Stack is empty"<<endl;
-				}
-				else{
-					if(top==0){
-						array[2k+1]=stack[top];
-						cout<<top<<endl;
-						top--;
-						k=2k+1;
-					}
-					else{
-							array[2k+2]=stack[top];
-							array[2k+1]=stack[top-1];
-							cout<<top<<top-1<<endl;
-							if (array[2k+1]=='+'||array[2k+1]=='-'||array[2k+1]=='*'||array[2k+1]=='/'){
-								k=2k+1;	
-							}
-							else if (array[2k+2]=='+'||array[2k+2]=='-'||array[2k+2]=='*'||array[2k+2]=='/'){
-								k=2k+2;
-							}
-					}
-				}
-			}
-			else{
-				if (top==-1){
-					cout<<"Stack is empty"<<endl;
-				}
-				else{
-					++top;
-					stack[top]=data;
-				}
-			}
-		} 
-}e;
-int main(){
-	string postfix="AB+";
-	string revpostfix;
-	for (int i = postfix.length() - 1; i >= 0; i--) {
-        revpostfix += postfix[i];
+
+class exptree {
+public:
+    char array[100];
+    char stack[10];
+    int top = -1, k = 0;
+
+    void root(char val) {
+        array[0] = val;
     }
-	e.root(revpostfix[0]);
-	for (int i=1;i<revpostfix.length();i++){
-		e.exp(revpostfix[i]);
-	}
+
+    void exp(char data) {
+        if (data == '+' || data == '-' || data == '*' || data == '/') {
+            if (top == -1) {
+                cout << "Stack is empty" << endl;
+                return;
+            }
+            else {
+                if (top == 0) {
+                    array[2 * k + 1]=data;
+                    array[2 * k + 2] = stack[top];
+                    top--;
+                    k = 2 * k + 1;
+                }
+                else {
+                    array[2 * k + 1]=data;
+                    array[2 * k + 3] = stack[top];
+                    array[2 * k + 2] = stack[top - 1];
+                    if (array[2 * k + 1] == '+' || array[2 * k + 1] == '-' || array[2 * k + 1] == '*' || array[2 * k + 1] == '/') {
+                        k = 2 * k + 1;
+                    }
+                    else if (array[2 * k + 2] == '+' || array[2 * k + 2] == '-' || array[2 * k + 2] == '*' || array[2 * k + 2] == '/') {
+                        k = 2 * k + 2;
+                    }
+                    top -= 2;
+                }
+            }
+        }
+        else if(data == '?'){
+            array[2 * k + 2] = stack[top];
+            array[2 * k + 1] = stack[top - 1];
+        }
+        else {
+            if (top == 9) {
+                cout << "Stack is full" << endl;
+                return;
+            }
+            else {
+                top ++;
+                stack[top] = data;
+            }
+        }
+    }
+    void display() {
+        for (int i=0;i<100;i++){
+            /*if (array[i] == '\0') {
+                continue;
+            }*/
+            //else{
+                cout << array[i] << " ";
+            //}
+        }
+    }
+};
+
+int main() {
+    string postfix = "?AB/C*D*E+";
+    string revpostfix(postfix.rbegin(), postfix.rend());
+    exptree e;
+    e.root(revpostfix[0]);
+    for (int i = 1; i < postfix.length()-1; i++) {
+        e.exp(revpostfix[i]);
+    }
+    e.display();
+    return 0;
 }
 
-						
